5eee6e2a3e4b5c33c64f561540cfb5eb11d6d801
Alannah Webster <alannahwebster@alannahs-air.student.wireless.sydney.edu.au>
2023-04-03 10:46:11 +1000

    change in port

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ class SSLCherootAdapter(ServerAdapter):
 host = 'localhost'
 
 # Test port, change to the appropriate port to host
-port = 8080
+port = 8081
 
 # Turn this off for production
 debug = True
